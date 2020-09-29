9a72905d59a4197fd2ac75a36d21a677327de1b8
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-29 15:11:49 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Menumakanan from './Page/MenuMakanan';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Menumakanan />
+    </div>
+  );
+}
+
+export default App;
